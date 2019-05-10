885301d53ed38357b40b0c26536f6149c8758f14
Lucas Ferreira <ferreira.alves@live.com>
2019-05-09 21:46:53 -0300

    mock da listagem

---
+++ javascript.js
@@ -0,0 +1,14 @@
+users = {'users': []}
+console.log(users['users']);
+users['users'].push(
+  {
+    'name': 'lucas ferreira',
+    'id': 1,
+    'instrumentos': [
+      'guitarra',
+      'baixo',
+      'vocal'
+    ]
+  }
+)
+console.log(users['users'][0]);
