d129db2860b0abce75642fba9c8de609f11bfc65
BrunoHRF <BrunoHRF@outlook.com>
2019-06-04 16:25:22 -0300

    INDEV começando organização dos forms e funconalidade de mostrar e esconder

--- javascript.js
+++ javascript.js
@@ -1,14 +1,81 @@
-users = {'users': []}
-console.log(users['users']);
-users['users'].push(
-  {
-    'name': 'lucas ferreira',
-    'id': 1,
-    'instrumentos': [
-      'guitarra',
-      'baixo',
-      'vocal'
-    ]
+
+  let imagemcaraio = 10;
+
+  function imageHandler(e2) {
+      let store = document.getElementById('pic-goes-here');
+      store.innerHTML = '<img src="' + e2.target.result + '" style=" height: 134px" padding: 10 px>';
+      let jsonstore = e2.target.result;
+      imagemcaraio = jsonstore;
   }
-)
-console.log(users['users'][0]);
+
+  function loadimage(e1) {
+      var filename = e1.target.files[0];
+      var fr = new FileReader();
+      fr.onload = imageHandler;
+      fr.readAsDataURL(filename);
+  }
+
+
+  window.onload = function () {
+    var y = document.getElementById("inputImage");
+    y.addEventListener('change', loadimage, false);
+  }
+
+  $("#btnInsert").click(function () {
+    // Verfica se o formulário está preenchido corretamente
+    if (!$('#form-contato-musico')[0].checkValidity()) {
+      displayMessage("Preencha o formulário corretamente.");
+      return;
+    }
+
+    // Obtem os valores dos campos do formulário
+    let campoNome = $("#inputNome").val();
+    let campoEmail = $("#inputEmail").val();
+    let campoIdade = $("#inputIdade").val();
+    let campoEstado = $("#inputEstado").val();
+    let campoCidade = $("#inputCidade").val();
+    let campoInstrumentos = [];
+    $.each($("input[name='instrumentos']:checked"), function(){
+        campoInstrumentos.push($(this).val());
+    });
+    let campoEstilos = [];
+    $.each($("input[name='estilos']:checked"), function(){
+        campoEstilos.push($(this).val());
+    });
+    let campoDescicao = $("#inputDescricao").val();
+    let campoImagem = document.getElementById("inputImage");
+    usuario = {
+      "nome": campoNome,
+      "email": campoEmail,
+      "idade": campoIdade,
+      "cidade": campoCidade,
+      "estado": campoEstado,
+      "instrumentos": campoInstrumentos,
+      "estilos": campoEstilos,
+      "descricao": campoDescicao,
+      "image_path": imagemcaraio
+    };
+    insertUsuario(usuario);
+    location.href = 'lista.html';
+  }); 
+  
+  // Função para alternar qual forms vai ser mostrado
+  function cadastroMusico(){  
+    let showForms = document.getElementById("form-contato-musico");
+    if(showForms.style.display === "none"){
+      showForms.style.display = "block";
+    }
+    else{
+      showForms.style.display = "none";
+    }
+  }
+  function cadastroBanda(){  
+    let showForms = document.getElementById("form-contato-banda");
+    if(showForms.style.display === "none"){
+      showForms.style.display = "block";
+    }
+    else{
+      showForms.style.display = "none";
+    }
+  }
+
